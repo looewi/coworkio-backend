6be4779dd55b8af6c982c738bd60b74a6838d4f4
lowenzachn <kuchumova.mary@gmail.com>
2016-11-08 12:00:41 +0300

    add cors properties

---
+++ src/main/kotlin/com/coworkio/configuration/properties/CorsProperties.kt
@@ -0,0 +1,13 @@
+package com.coworkio.configuration.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "spring.security.cors")
+open class CorsProperties {
+
+    var origins: Array<String> = Array(0, {x -> ""})
+}
