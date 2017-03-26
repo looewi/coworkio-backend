4c48c015be2b871ef24af565d7383975f254966a
lowenzachn <kuchumova.mary@gmail.com>
2017-03-26 15:21:22 +0300

    change all rest api urls start with '/api'

--- src/main/kotlin/com/coworkio/Constants.kt
+++ src/main/kotlin/com/coworkio/Constants.kt
@@ -5,9 +5,10 @@ const val EMAIL_CLAIM = "email"
 const val KEY = "5E3b86m26SKXhet8d9Y1UVl2p62AUoYRhLmXd6S6mh7dM0AMd6LXEC22VHSVb7hk"
 const val SERVER_URL = "coworkio.io"
 
-const val AUTH_PREFIX = "/auth"
+const val API_CONSTANT = "/api"
+const val AUTH_PREFIX = "$API_CONSTANT/auth"
 
 const val HOST = "http://localhost:8080"
 
-const val AUTHENTICATE_URL = "/auth/login"
+const val AUTHENTICATE_URL = "$AUTH_PREFIX/login"
 const val HTTP_POST = "POST"
